3c27389ef6f770f15fcf4ef099ae0f34a4a672c1
artur.trombim <artur.trombim@itravel.com.br>
2024-08-02 17:32:37 -0300

    start project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            WeCallForU.WeCallForU api = new WeCallForU.WeCallForU();
+            WeCallForU.Context context = new WeCallForU.Context();
+
+            context.Body = @"<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:ws='http://ws.travelsuite.com.br'>
+   <soapenv:Header/>
+   <soapenv:Body>
+      <ws:GetPnrGroupDetail>
+         <!--Optional:-->
+         <ws:pnrGroup>R9L9J8B579</ws:pnrGroup>
+      </ws:GetPnrGroupDetail>
+   </soapenv:Body>
+</soapenv:Envelope>";
+            context.RequestType= WeCallForU.Context.Type.Post;
+            context.Url= "http://webservices.travelexplorer.com.br/reservation/resservice/wsReservation.asmx";
+            string teste = await api.Execute(context);
+        }
+    }
+}
---
+++ WeCallForU/WeCallForU.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeCallForU
+{
+    public class WeCallForU
+    {
+        /// <summary>
+        /// Envia uma requisição HTTP POST para a URL especificada no contexto e retorna a resposta como uma string.
+        /// </summary>
+        /// <param name="context">Objeto que contém o corpo da requisição, cabeçalhos personalizados e URL.</param>
+        /// <returns>Uma tarefa que representa a operação assíncrona, contendo a resposta da requisição como uma string.</returns>
+        /// <exception cref="HttpRequestException">Lançada quando ocorre um erro durante a requisição HTTP.</exception>
+        public async Task<string> Execute(Context context)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpContent content = new StringContent(context.Body, Encoding.UTF8, "text/xml");
+
+                if (context.Header != null) {
+                    foreach (var item in context.Header)
+                    {
+                        content.Headers.Add(item.Key, item.Value);
+                    } 
+                }
+                try
+                {
+                    HttpResponseMessage response = await client.PostAsync(context.Url, content);
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync();
+
+                }catch(HttpRequestException e)
+                {
+                    return "Erro na requisição HTTP: " + e.Message;
+                }
+
+            }
+        }
+    }
+    /// <summary>
+    /// Representa um contexto de requisição HTTP.
+    /// </summary>
+    /// <param name="Url">A URL da requisição. (Tipo: string)</param>
+    /// <param name="Body">O corpo da requisição. (Tipo: string)</param>
+    /// <param name="Header">Os cabeçalhos da requisição. (Tipo: Dictionary&lt;string, string&gt;)</param>
+    /// <param name="RequestType">O tipo da requisição (GET ou POST). (Tipo: Context.Type)</param>
+    public class Context{
+        public string Url { get; set; }
+        public string Body { get; set; }
+        public Dictionary<string,string> Header { get; set; }
+        public Type RequestType { get; set; }
+        public enum Type
+        {
+            Get,
+            Post
+        }
+    }
+
+}
